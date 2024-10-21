2d7f54f58dde54b0278ec6852d47e367c7049831
Nikola Dragomirović <132782535+nikoladragomirovic@users.noreply.github.com>
2024-10-22 00:05:45 +0200

    Add Splash Screens And Animations

--- app.vue
+++ app.vue
@@ -1,17 +1,47 @@
 <template>
-  <Tally v-if="step === steps['tally']" :userId="userId"></Tally>
-  <Calendly v-if="step === steps['calendly']" :prefill="prefill"></Calendly>
+  <Transition>
+    <Step v-if="step === steps['splash1']" :step="1" :title="'FORM'"></Step>
+  </Transition>
+  <Tally
+    v-if="step < 3"
+    :class="{ hidden: step !== steps['tally'], show: step === steps['tally'] }"
+    :userId="userId"
+  ></Tally>
+  <Transition>
+    <Step v-if="step === steps['splash2']" :step="2" :title="'CALENDLY'"></Step>
+  </Transition>
+  <Calendly
+    v-if="step > 2"
+    :class="{
+      hidden: step !== steps['calendly'],
+      show: step === steps['calendly'],
+    }"
+    :prefill="prefill"
+  ></Calendly>
 </template>
 
 <script setup lang="ts">
 import { uuid } from "vue-uuid";
 
-const steps = {
-  tally: 1,
-  calendly: 2,
+const makeTransition: any = (
+  from: keyof typeof steps,
+  to: keyof typeof steps
+) => {
+  step.value = steps[from];
+  setTimeout(() => {
+    step.value = steps[to];
+  }, 1500);
 };
 
-const step = ref(steps["tally"]);
+const steps = {
+  initial: 0,
+  splash1: 1,
+  tally: 2,
+  splash2: 3,
+  calendly: 4,
+};
+
+const step = ref(steps["initial"]);
 const userId = useState("userId", () => uuid.v4());
 const prefill = ref({});
 
@@ -24,9 +54,11 @@ onMounted(() => {
     prefill.value = JSON.parse(prefillData);
   }
 
-  const savedStep = localStorage.getItem("currentStep");
-  if (savedStep) {
+  const savedStep = Number(localStorage.getItem("currentStep"));
+  if (savedStep === steps["tally"] || savedStep === steps["calendly"]) {
     step.value = Number(savedStep);
+  } else {
+    makeTransition("splash1", "tally");
   }
 
   ws.addEventListener("open", () => {
@@ -35,15 +67,44 @@ onMounted(() => {
 
   ws.addEventListener("message", (event) => {
     prefill.value = JSON.parse(event.data).message;
-    step.value = steps["calendly"];
+    makeTransition("splash2", "calendly");
   });
 });
 
 watch(step, (update) => {
-  localStorage.setItem("currentStep", update);
+  localStorage.setItem("currentStep", update.toString());
 });
 
 watch(prefill, (update) => {
   localStorage.setItem("prefillData", JSON.stringify(update));
 });
 </script>
+
+<style scoped>
+.v-enter-active,
+.v-leave-active {
+  transition: all 1s ease;
+}
+
+.v-enter-from {
+  opacity: 0;
+  transform: translateY(-200px);
+}
+.v-leave-to {
+  opacity: 0;
+  transform: translateY(200px);
+}
+
+.hidden {
+  opacity: 0;
+  position: absolute;
+  top: -100%;
+  transform: translateY(-200px);
+}
+
+.show {
+  opacity: 1;
+  position: relative;
+  transition: all 1s ease 1.5s;
+}
+</style>
---
+++ components/step.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="step-container">
+    <p class="step-title">
+      STEP {{ props.step }} -
+      {{ props.title }}
+    </p>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({ step: Number, title: String });
+</script>
+
+<style scoped>
+.step-container {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.step-title {
+  font-family: Cambria, Cochin, Georgia, Times, "Times New Roman", serif;
+  font-size: 24px;
+}
+</style>
